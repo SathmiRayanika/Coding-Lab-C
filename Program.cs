348d04346445cf726119aff8e42f1d0018fcf01b
ES Rayanika <139951996+SathmiRayanika@users.noreply.github.com>
2024-02-05 23:40:40 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,42 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp7
+namespace quetion_05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter first number");
-            double n1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter second number");
-            double n2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Enter operator:");
-            string op = Console.ReadLine();
+            int num;
+            Console.Write("Test Data: ");
+            num = Convert.ToInt32(Console.ReadLine());
 
-            double result = 0;
-            bool valid = true;
-            switch (op)
+            Console.WriteLine("Expected Output:");
+            if (num >= 0)
             {
-                case "+";
-                    result = n1 + n2;
-                    break;
-                case "-";
-                    result = n1 - n2;
-                    break;
-                case "*";
-                    result = n1 * n2;
-                    break;
-                case "/";
-                    result = n1 / n2;
-                    break;
-                default:
-                    valid = false;
-                    break;
-
+                Console.WriteLine("{0} is a positive number.", num);
             }
-            if (valid) Console.WriteLine("Result is:" + result);
-            else Console.WriteLine("Invalid operator!");
+            else
+            {
+                Console.WriteLine("{0} is a negative number", num);
+            }
+
             Console.ReadLine();
         }
     }
