7f6575d7895d3dcc301a63d3d770c35acdabc6d7
ES Rayanika <139951996+SathmiRayanika@users.noreply.github.com>
2024-02-16 22:30:08 +0530

    Positive Negative Number

--- Program.cs
+++ Program.cs
@@ -4,31 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp8
+namespace quetion_05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter the title of the book: ");
-            string title = Console.ReadLine();
+            int num;
+            Console.Write("Test Data: ");
+            num = Convert.ToInt32(Console.ReadLine());
 
-            Console.Write("Enter the author of the book: ");
-            string author = Console.ReadLine();
+            Console.WriteLine("Expected Output:");
+            if (num >= 0)
+            {
+                Console.WriteLine("{0} is a positive number.", num);
+            }
+            else
+            {
+                Console.WriteLine("{0} is a negative number", num);
+            }
 
-            Console.Write("Enter the publication year of the book: ");
-            int publicationYear = int.Parse(Console.ReadLine());
-
-            Console.Write("Enter the ISBN of the book: ");
-            string isbn = Console.ReadLine();
-
-
-            Console.WriteLine("\nBook Information:");
-            Console.WriteLine("-------------------");
-            Console.WriteLine("Title: {title}");
-            Console.WriteLine("Author: {author}");
-            Console.WriteLine("Publication Year: {publicationYear}");
-            Console.WriteLine("ISBN: {isbn}");
             Console.ReadLine();
         }
     }
