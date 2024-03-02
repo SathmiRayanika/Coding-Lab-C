0118d8171756983bbed69a2a965eade1b0bc8ed6
ES Rayanika <139951996+SathmiRayanika@users.noreply.github.com>
2024-03-03 00:40:21 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyProject2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnString_Click(object sender, EventArgs e)
+        {
+            string fname;
+            string messageText;
+
+            messageText = "Your name is:";
+
+            fname = txtStrings.Text;
+            MessageBox.Show(messageText + fname);
+
+            textBox1.Text = messageText+fname;
+        }
+    }
+}
